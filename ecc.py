a1249fd6fc09f079289cc0435fe9f95576adf4c4
Justin Moore <humanumbrella@gmail.com>
2019-09-15 21:05:37 -0700

    working through ch2

--- ecc.py
+++ ecc.py
@@ -9,15 +9,67 @@ class Point:
         self.b = b
         self.x = x
         self.y = y
-        if self.y**2 != self.x**3 + a * x + b:
+        if self.x is None and self.y is None:
+            return
+        if self.y**2 != self.x**3 + self.a * self.x + self.b:
             raise ValueError('({},{}) is not on the curve.'.format(x, y))
 
     def __eq__(self, other):
-        return self.x == other.x and self.y == other.y \
-            and self.a == other.a and self.b == other.b
+        return (self.x == other.x and self.y == other.y
+                and self.a == other.a and self.b == other.b)
 
-    def __neq__(self, o):
-        return not(self == o)  # This would mean self.num != o.num and self.prime != o.prime
+    def __ne__(self, other):
+        return (self.x != other.x or self.y != other.y
+                or self.a != other.a or self.b != other.b)
+
+    def __add__(self, other):
+        if self.a != other.a or self.b != other.b:
+            raise TypeError('({}, {}) is not on the same curve.'.format(self, other))
+
+        if self.x is None:
+            return other
+        if other.x is None:
+            return self
+
+        # if self == other and self.y == 0 * self.x:
+        #     return
+
+        if self is other:
+            # tangent line to this point.
+            # One more special case here if we're at y == 0, should return
+            # the point at infinity.
+            # if self.y == (0 * self.x):
+            #    return self.__class__(None, None, self.a, self.b)
+            s = (3 * self.x**2 + self.a) / (2 * self.y)
+            x_3 = s**2 - 2 * self.x
+            y_3 = s * (self.x - x_3) - self.y
+            return self.__class__(x_3, y_3, self.a, self.b)
+
+        # This is point addition via slope and reflection over x-axis
+        if self.x is not other.x:
+            s = (other.y - self.y) / (other.x - self.x)
+            x_3 = s**2 - self.x - other.x
+            y_3 = s * (self.x - x_3) - self.y
+            return self.__class__(x_3, y_3, self.a, self.b)
+
+        if self.x == other.x and self.y != other.y:  # additive inverse
+            return self.__class__(None, None, self.a, self.b)
+
+    def __rmul__(self, scalar_multiple):
+        current = self
+        result = self.__class__(None, None, self.a, self.b)
+        while scalar_multiple:
+            if scalar_multiple & 1:
+                result += current
+            current += current
+            scalar_multiple >>= 1
+        return result
+
+    # def __rmul__(self, scalar_multiple):
+    #     product = self.__class__(None, None, self.a, self.b)
+    #     for _ in range(scalar_multiple):
+    #         product += self
+    #     return product
 
 
 class FieldElement:
@@ -27,7 +79,7 @@ class FieldElement:
             error = 'Num {} not in field range 0 to {}'.format(num, prime-1)
             raise ValueError(error)
         self.num = num
-        # FIXZME -- is prime prime? lol
+        # FIXME -- is prime prime? lol
         self.prime = prime
 
     def __repr__(self):
@@ -38,8 +90,9 @@ class FieldElement:
             return False
         return self.num == o.num and self.prime == o.prime
 
-    def __neq__(self, o):
-        return not(self == o)  # This would mean self.num != o.num and self.prime != o.prime
+    def __ne__(self, o):
+        return (self.num != o.num or self.prime != o.prime)
+        # return self is not o  # This would mean self.num != o.num and self.prime != o.prime
 
     def __add__(self, o):
         if self.prime != o.prime:
@@ -73,14 +126,29 @@ class FieldElement:
         num = self.num * pow(o.num, self.prime-2, self.prime) % self.prime
         return self.__class__(num, self.prime)
 
-    def __floordiv__(self, o):
-        if self.prime != o.prime:
-            error = 'Cannot divide two numbers in different prime fields.'
-            raise TypeError(error)
-        pass
-
     def __pow__(self, exponent):
         # Exponent is just a regular number not a FieldElement.
         n = exponent % (self.prime - 1)
         num = pow(self.num, n, self.prime)
         return self.__class__(num, self.prime)
+
+
+class ECCTest():
+
+    def test_on_curve(self):
+        prime = 223
+        a = FieldElement(0, prime)
+        b = FieldElement(7, prime)
+        valid_points = ((192, 105), (17, 56), (1, 193))
+        invalid_points = ((200, 119), (42, 99))
+
+        for x_plain, y_plain in valid_points:
+            x = FieldElement(x_plain, prime)
+            y = FieldElement(y_plain, prime)
+            Point(x, y, a, b)
+
+        for x_plain, y_plain in invalid_points:
+            x = FieldElement(x_plain, prime)
+            y = FieldElement(y_plain, prime)
+            with self.assertRaises(ValueError):
+                Point(x, y, a, b)
--- test.py
+++ test.py
@@ -1,27 +1,50 @@
 from ecc import FieldElement, Point
 
-# p1 = Point(2, 4, 5, 7)
-p2 = Point(-1, -1, 5, 7)
-p3 = Point(18, 77, 5, 7)
+prime = 223
+a = FieldElement(num=0, prime=prime)
+b = FieldElement(num=7, prime=prime)
+x = FieldElement(num=47, prime=prime)
+y = FieldElement(num=71, prime=prime)
+p = Point(x, y, a, b)
 
-a = FieldElement(7, 13)
-b = FieldElement(12, 13)
-print(a, b, a is b)
-print(a, a, a is a)
-print(a, b, a != b)
-print(a, a, a != a)
-c = FieldElement(6, 13)
-print(a, b, c, a + b == c)
+for s in range(1, 21):
+    result = s*p
+    print('{}*(47,71)=({},{})'.format(s, result.x.num, result.y.num))
 
-aa = FieldElement(3, 13)
-bb = FieldElement(12, 13)
-cc = FieldElement(10, 13)
 
-print(aa, bb, cc, aa*bb == cc)
+# prime = 223
+# a = FieldElement(num=0, prime=prime)
+# b = FieldElement(num=7, prime=prime)
+# x1 = FieldElement(num=192, prime=prime)
+# y1 = FieldElement(num=105, prime=prime)
+# x2 = FieldElement(num=17, prime=prime)
+# y2 = FieldElement(num=56, prime=prime)
+# p1 = Point(x1, y1, a, b)
+# p2 = Point(x2, y2, a, b)
+# print(p1+p2)
 
-dd = FieldElement(1, 13)
+# # p1 = Point(2, 4, 5, 7)
+# p2 = Point(-1, -1, 5, 7)
+# p3 = Point(18, 77, 5, 7)
 
-print(aa, dd, aa*dd == aa)
+# a = FieldElement(7, 13)
+# b = FieldElement(12, 13)
+# print(a, b, a is b)
+# print(a, a, a is a)
+# print(a, b, a != b)
+# print(a, a, a != a)
+# c = FieldElement(6, 13)
+# print(a, b, c, a + b == c)
 
-print(aa, dd, aa**3 == dd)
-print(aa, dd, aa**-3 == dd)
+# aa = FieldElement(3, 13)
+# bb = FieldElement(12, 13)
+# cc = FieldElement(10, 13)
+
+# print(aa, bb, cc, aa*bb == cc)
+
+# dd = FieldElement(1, 13)
+
+# print(aa, dd, aa*dd == aa)
+
+# print(aa, dd, aa**3 == dd)
+# print(aa, dd, aa**-3 == dd)
